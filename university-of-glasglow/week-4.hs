755c96bed2f7e1363b2a77e519a57443d36f1f05
aportno <alexander.portno@gmail.com>
2021-05-16 02:22:12 -0400

    update

--- university-of-glasglow/week-4.hs
+++ university-of-glasglow/week-4.hs
@@ -1,3 +1,4 @@
+-- using let-in
 journeyCost :: Float -> Float -> Float
 journeyCost miles fuelCostPerLitre =
     let milesPerGallon = 35
@@ -5,12 +6,44 @@ journeyCost miles fuelCostPerLitre =
         gallons = miles / milesPerGallon
     in (gallons * litresPerGallon * fuelCostPerLitre)
 
+-- using where
 squarePlusOne :: Int -> Int
 squarePlusOne x = xSquared + 1
     where xSquared = x*x
 
+-- using multiple where statements
 celsToFahr :: Float -> Float
 celsToFahr deg = (deg*scalingFactor) + freezingPoint
     where scalingFactor = 9.0/5.0
           freezingPoint = 32
+
+-- using if-then-else statements
+absolute :: (Ord p, Num p) => p -> p
+absolute x = if x < 0 then (-x) else x
+
+-- using guards
+absolute' :: (Ord p, Num p) => p -> p
+absolute' x
+    | x < 0 = -x
+    | otherwise = x
+
+-- using guards and a where statement
+holeScore :: Int -> Int -> String
+holeScore strokes par
+    | score < 0 = show (abs score) ++ " under par"
+    | score == 0 = "level par"
+    | otherwise = show score ++ " over par"
+    where score = strokes - par
+
+-- algebraic data types
+data Pet = Cat | Dog | Fish | Parrot String
+
+-- case expression
+hello :: Pet -> [Char]
+hello animal =
+    case animal of
+        Cat -> "meeow"
+        Dog -> "woof"
+        Fish -> "bubble"
+        Parrot name -> "pretty " ++ name
 
